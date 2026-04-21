4c57059b623ab0e280fd7571247234b76305e675
AnnishLitisha09 <jannishlitisha2005@gmail.com>
2026-04-21 19:02:22 +0530

    fix

--- migrations/20260325065059-add-stage-to-tasks.js
+++ migrations/20260325065059-add-stage-to-tasks.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Tasks', 'stage', {
+    await queryInterface.addColumn('tasks', 'stage', {
       type: Sequelize.STRING,
       allowNull: false,
       defaultValue: 'Active'
@@ -11,6 +11,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Tasks', 'stage');
+    await queryInterface.removeColumn('tasks', 'stage');
   }
 };
